7be56c1eaf469432680e314c2e677f6cff11db4d
mongru <monika.grubizna@gmail.com>
2017-08-08 15:49:05 +0200

    add font variables, main-info scss fixes

--- js/components/main__info.jsx
+++ js/components/main__info.jsx
@@ -13,9 +13,9 @@ export class MainInfo extends React.Component {
     handleScrollText = (event) => {
         // console.log(event);
         if (this.scrollText !== null) {
-            if(($(document).scrollTop() + $(window).height() / 8) > ($('body').height() / 8)){
+            if (($(document).scrollTop() + $(window).height() / 8) > ($('body').height() / 8)) {
                 $(this.scrollText).attr('class', 'text-beforeScroll');
-            }else{
+            } else {
                 $(this.scrollText).attr('class', 'text-afterScroll');
             }
         }
@@ -24,9 +24,9 @@ export class MainInfo extends React.Component {
     handleScrollStripe = (event) => {
         // console.log(event);
         if (this.scrollStripe !== null) {
-            if(($(document).scrollTop() + $(window).height() / 2) > ($('body').height() / 2)){
+            if (($(document).scrollTop() + $(window).height() / 2) > ($('body').height() / 2)) {
                 $(this.scrollStripe).attr('class', 'stripe-beforeScroll');
-            }else{
+            } else {
                 $(this.scrollStripe).attr('class', 'stripe-afterScroll');
             }
         }
@@ -36,53 +36,39 @@ export class MainInfo extends React.Component {
         window.addEventListener('scroll', this.handleScrollText);
         window.addEventListener('scroll', this.handleScrollStripe);
 
-
-
-
-
-        // let scrollTop = event.srcElement.body.scrollTop,
-        // itemTranslate = Math.min(0, scrollTop/3 - 60);
     }
-
-    // this.setState ({
-    //     transform: itemTranslate
-    // });
-
-// }
 
     componentWillUnmount() {
         window.removeEventListener('scroll', this.handleScrollText);
         window.removeEventListener('scroll', this.handleScrollStripe);
     }
 
+    render(){
+        
+        return (
+            <main>
+                <section className="main">
+                    <div className="container main__info">
+                        <div className="row">
+                            <div className="col-12 main__info--banner">
+                                <h1 ref={(ref) => this.scrollText = ref}>
+                                    Pay it forward <br/>
+                                    expat community
+                                </h1>
+                                <p>
+                                    Meet like-minded people in your new area <br/>
+                                    willing to share their knowledge, struggles and experience
+                                </p>
 
-  render(){
-
-
-    return (
-        <main>
-            <section className="main">
-                <div className="container main__info">
-                    <div className="row">
-                        <div className="col-12 main__info--banner">
-                            <h1 ref={(ref) => this.scrollText = ref}>
-                                Pay it forward <br/>
-                                expat community
-                            </h1>
-                            <p>
-                                Meet like-minded people in your new area <br/>
-                                willing to share their knowledge, struggles and experience
-                            </p>
+                            </div>
 
                         </div>
-
+                        <div ref={(ref) => this.scrollStripe = ref} className="main__info--stripe"></div>
+                        <div className="main__info--circle"></div>
                     </div>
-                    <div ref={(ref) => this.scrollStripe = ref} className="main__info--stripe"></div>
-                    <div className="main__info--circle"></div>
-                </div>
 
-            </section>
-        </main>
-    );
-  }
+                </section>
+            </main>
+        );
+    }
 }
