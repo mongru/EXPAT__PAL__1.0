00a42f3b95f59eae897b5b46eef78269e0ccdd11
mongru <monika.grubizna@gmail.com>
2017-08-28 14:19:06 +0200

    firebase check

--- js/components/main__searchform.jsx
+++ js/components/main__searchform.jsx
@@ -32,31 +32,31 @@ export class MainSearchForm extends React.Component {
 
     render() {
 
-    return (
-        <section id="main__searchform" className="main__searchform">
-            <div className="container main__searchform--container">
-                <div className="row">
-                    <div className="col-12">
-                        <form className="main__searchform--form" onSubmit={(e) => this.addLocation(e).bind(this)}>
-                            <fieldset>
-                                <legend className="main__searchform--title">Find people in your area</legend>
-                                <input id="pac-input" type="text" ref={ el => this.inputEl = el }
-                                    placeholder="Enter your location"/>
-                            </fieldset>
-                            <button onClick={(e) => this.addLocation(e)} className="main__searchform--button">Submit</button>
-                        </form>
+        return (
+            <section id="main__searchform" className="main__searchform">
+                <div className="container main__searchform--container">
+                    <div className="row">
+                        <div className="col-12">
+                            <form className="main__searchform--form" onSubmit={(e) => this.addLocation(e).bind(this)}>
+                                <fieldset>
+                                    <legend className="main__searchform--title">Find people in your area</legend>
+                                    <input id="pac-input" type="text" ref={ el => this.inputEl = el }
+                                        placeholder="Enter your location"/>
+                                </fieldset>
+                                <button onClick={(e) => this.addLocation(e)} className="main__searchform--button">Submit</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div id="map"></div>
-            <div id="infowindow-content">
-                <img src="" width="16" height="16" id="place-icon"/>
-                <span id="place-name"  className="title"></span><br/>
-                <span id="place-address"></span>
-            </div>
+                <div id="map"></div>
+                <div id="infowindow-content">
+                    <img src="" width="16" height="16" id="place-icon"/>
+                    <span id="place-name"  className="title"></span><br/>
+                    <span id="place-address"></span>
+                </div>
 
-        </section>
-    );
+            </section>
+        );
     }
 }
