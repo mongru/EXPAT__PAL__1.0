62f5b66f73a05a2028345658501abe77dfe536f8
mongru <monika.grubizna@gmail.com>
2018-01-24 21:52:47 +0100

    console frog saves the day

--- js/app.jsx
+++ js/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../scss/main.scss';
+//console fun
+import 'console-dot-frog';
+
 
 // -------> COMPONENTS
 import { Template } from './components/template.jsx';
@@ -8,7 +11,8 @@ import { Template } from './components/template.jsx';
 class App extends React.Component {
 
   render(){
-      console.log("Welcome! Happy to see you here :) I'm still learning and I'd love to find a place where I could work, learn and become a great front-end dev one day. If you're looking for a nice and motivated person to grow your team or just simply feel like saying 'hello', don't hesitate to drop me a line: monika.grubizna@gmail.com");
+      console.log('%cWelcome! Happy to see you here :) I\'m still learning and I\'d love to find a place where I could work, learn and become a great front-end dev one day. If you\'re looking for a nice and motivated person to grow your team or just simply feel like saying \'hello\', don\'t hesitate to drop me a line:', 'font-size: 1.2em; line-height: 2em; color: blue;');
+      console.frog('monika.grubizna@gmail.com');
 
       return (<Template/>);
   }
